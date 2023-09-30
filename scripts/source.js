0d4c96cf5d85161c00da494a301ba7c082fa463f
swapinga <galgaztin@gmail.com>
2023-09-30 19:05:13 +1000

    Uso de spread + Estadisticas + LocalStorage

--- scripts/source.js
+++ scripts/source.js
@@ -2,8 +2,16 @@ let dados = 0;
 let sumatotal = 0;
 let sumadealer = 0;
 let dadosdealer = 0;
-let dadosplayer = 0;
+let dadosplayer = 0; 
+let winrate = 0;
+let cantidadPartidas = 0;
+let partidasGanadas = 0;
+let partidasPerdidas = 0;   
 const registro = [];
+let recuperarDatos = localStorage.getItem("partida")
+if (recuperarDatos !== null) {
+    registro.unshift(...JSON.parse(recuperarDatos)); //Uso de spread para desarmar el array de arrays
+  }
 function blackjack() {
     document.querySelector("#botontirar").addEventListener("click", function () {
         dados = parseInt(document.getElementById("numero").value);
@@ -20,16 +28,21 @@ function blackjack() {
         else if (sumadealer === 0){alert("El numero de dados debe ser mayor a 0")}
         if (sumatotal > 21 && sumadealer === 0){
             alert("Te pasaste de 21! El dealer gana")
-            registro.push([false,sumatotal,sumadealer])
-            reset()
+            registro.push([0,sumatotal,sumadealer])
+            document.getElementById("botonreset").hidden = false;
+            document.getElementById("botonstats").hidden = false;
+            localStorage.setItem("partida", JSON.stringify(registro));
+            estadisticas()
         }
     })
 }
 
-blackjack();
+
 
 function dealer() {
     document.querySelector("#botonquedarse").addEventListener("click", function () {
+        document.querySelector('#botontirar').disabled = true;
+        document.querySelector('#botonquedarse').disabled = true;
         let interval = setInterval(function () {
             if (sumadealer >= 21 || sumadealer > sumatotal) {
                 clearInterval(interval);
@@ -44,23 +57,43 @@ function dealer() {
 
     function mostrarResultadoDealer() {
         document.getElementById("resultadodealer").innerHTML = "El dealer tiro " + dadosdealer + " dados y sumo " + sumadealer + " puntos";
-
         if (sumadealer > 21) {
             alert("El dealer se pasó de 21. ¡Ganaste!");
-            registro.push([true,sumatotal,sumadealer])
-            reset()
+            registro.push([1,sumatotal,sumadealer])
         } else if (sumadealer >= sumatotal) {
             alert("Tu puntaje: " + sumatotal + ". El dealer sacó: " + sumadealer + ". El dealer gana");
-            registro.push([false,sumatotal,sumadealer])
-            reset()
+            registro.push([0,sumatotal,sumadealer])
         } else {
             alert("Tu puntaje: " + sumatotal + ". El dealer sacó: " + sumadealer + ". ¡Ganaste!");
-            registro.push([true,sumatotal,sumadealer])
-            reset()
+            registro.push([1,sumatotal,sumadealer])
         }
+        document.getElementById("botonreset").hidden = false;
+        document.getElementById("botonstats").hidden = false;
+        localStorage.setItem("partida", JSON.stringify(registro));
+        estadisticas();
     }
 }
+function estadisticas(){
+cantidadPartidas = registro.length;
+partidasGanadas = registro.reduce((sum, innerArray) => {
+   return sum + innerArray[0];}, 0);
+partidasPerdidas = cantidadPartidas - partidasGanadas;
+winrate = parseInt((partidasGanadas / partidasPerdidas) * 100)
+document.getElementById("partidaN").innerHTML = "Esta es la partida #" + cantidadPartidas;
+};
+
+
+    
+function stats() {
+    document.querySelector("#botonstats").addEventListener("click", function () {
+        alert("Partidas jugadas: " + cantidadPartidas + " Victorias: " + partidasGanadas + " Derrotas " + partidasPerdidas + " %Victoria " + winrate + "%")
+    })
+    };
+
+
+
 function reset() {
+    document.querySelector("#botonreset").addEventListener("click", function () {
     dados = 0;
     sumatotal = 0;
     sumadealer = 0;
@@ -68,7 +101,17 @@ function reset() {
     dadosplayer = 0;
     currentSum = 0;
     console.log(registro); //el registro de todas las partidas jugadas sin recargar la pagina
-}
+    document.getElementById("resultadodealer").innerHTML ="";
+    document.getElementById("resultadobox").innerHTML ="";
+    document.querySelector('#botonquedarse').disabled = false;
+    document.querySelector('#botontirar').disabled = false;
+    document.getElementById("botonreset").hidden = true;
+})}
 
 
+
+    estadisticas();
+    stats();
+    reset();
     dealer();
+    blackjack();
